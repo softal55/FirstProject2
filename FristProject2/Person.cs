41392f18f560688bdbe20a1f0287cd28578e24a8
Sofiane taleb <str14.don@gmail.com>
2021-03-25 23:06:04 +0100

    Add project files.

---
+++ FristProject2/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FristProject2
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public bool HasPet;
+
+        public void Greeting()
+        {
+            Console.WriteLine("Hi! My name is " + Name + " and my age is " + Age);
+        }
+    }
+}
---
+++ FristProject2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FristProject2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            var person = new Person();
+
+            person.Name = "Sofiane";
+            person.Age = 16;
+            person.HasPet = true;
+
+            person.Greeting();
+        }
+    }
+}
