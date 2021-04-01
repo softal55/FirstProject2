a4bde9e257228b2dbdcdf47ce41a3c40b84a56d2
Sofiane taleb <str14.don@gmail.com>
2021-04-01 21:40:15 +0200

    Add file 'AddingNumbers.cs'

---
+++ ExampleOne/AddingNumbers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleOne
+{
+    class AddingNumbers
+    {
+        public int Sum(int no1, int no2)
+        {
+            int add = no1 + no2;
+            return add;
+        }
+    }
+}
--- ExampleOne/Program.cs
+++ ExampleOne/Program.cs
@@ -6,24 +6,18 @@ namespace ExampleOne
     {
         public static void Main()
         {
-
-            int num1 = 0, num2 = 0;
+            
 
             Console.Write("Enter First number:");
-            num1 = Convert.ToInt32(Console.ReadLine());
-
+            int num1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter the second number:");
-            num2 = Convert.ToInt32(Console.ReadLine());
-            
+            int num2= Convert.ToInt32(Console.ReadLine());
             Program program = new Program();
-            int add = program.sum(num1, num2);
+            int add = program.Sum(num1, num2);
             Console.WriteLine("Addition:{0}" , add);
+            
         }
 
-        public int sum(int no1, int no2)
-        {
-            int add = no1 + no2;
-            return add;
-        }
+        
     }
 }
