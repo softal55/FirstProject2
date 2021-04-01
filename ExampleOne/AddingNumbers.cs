09767edf2934f465370fa8be46e8cd0c745f860c
mfaci2000 <mfaci2000@yahoo.com>
2021-04-01 15:30:50 -0500

    Add comments to 'AddingNumbers.cs' and 'Program.cs'

--- ExampleOne/AddingNumbers.cs
+++ ExampleOne/AddingNumbers.cs
@@ -6,10 +6,13 @@ namespace ExampleOne
 {
     class AddingNumbers
     {
+
+        // this method return a value of type int
+        // int Sum means the method Sum return a value of type int
         public int Sum(int no1, int no2)
         {
             int add = no1 + no2;
-            return add;
+            return add; // this is value that this method return to the caller (in this case 'Main' called this method)
         }
     }
 }
--- ExampleOne/Program.cs
+++ ExampleOne/Program.cs
@@ -12,8 +12,23 @@ namespace ExampleOne
             int num1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter the second number:");
             int num2= Convert.ToInt32(Console.ReadLine());
-            Program program = new Program();
-            int add = program.Sum(num1, num2);
+
+            // Since the method exists in another class, which is 'AddingNumbers'
+            // Also since the method is not static, then we need to create an instance of that class
+            //
+            // Create an instance called addition
+            AddingNumbers addition = new AddingNumbers();
+
+            // now that we have the instance, we can use it like this
+            int add = addition.Sum(num1, num2);
+            // So, this means call the method 'Sum' which is declared in the class 'AddingNumbers'
+            // and when the method adds the 2 numbers, it return the sum.
+            // the sum is then stored in the variable add.
+
+
+
+            //Program program = new Program();
+            //int add = program.Sum(num1, num2);
             Console.WriteLine("Addition:{0}" , add);
             
         }
